904310531d29d733b51adf3b5a2b7dd9a44f155b
Anchit Jain <anchitjain1234@gmail.com>
2020-05-02 17:34:54 +0530

    Updates

---
+++ precache-manifest.f65d070f15bc1c5d5bea35528ec289f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a870e0d81fe6af9c39e2e6ecfc027eb",
+    "url": "/covidapp/index.html"
+  },
+  {
+    "revision": "6b7429d0bb06ed3513bc",
+    "url": "/covidapp/static/css/main.ee05a449.chunk.css"
+  },
+  {
+    "revision": "6c4b2c2d4612da64bf77",
+    "url": "/covidapp/static/js/2.e1bae7a5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidapp/static/js/2.e1bae7a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b7429d0bb06ed3513bc",
+    "url": "/covidapp/static/js/main.ef5986aa.chunk.js"
+  },
+  {
+    "revision": "244004ff8caee94fd52c",
+    "url": "/covidapp/static/js/runtime-main.9da8f619.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidapp/static/media/image.d7265326.png"
+  }
+]);
